f809f7d972059e6115e709f9832de0b32b96f6ca
shiyabin <31621564+shiyabin@users.noreply.github.com>
2022-09-15 18:26:40 +0800

    fix: 删除打印信息

--- src/libs/webpack-plugin.ts
+++ src/libs/webpack-plugin.ts
@@ -7,7 +7,6 @@ export default class TaosDataUploadFileWebpack {
     this.config = config
   }
   apply(complier: Compiler) {
-    console.log('complier', complier)
     complier.hooks.done.tap(
       'TaosDataUploadFileWebpack',
       (stats /* 在 hook 被触及时，会将 stats 作为参数传入。 */) => {
